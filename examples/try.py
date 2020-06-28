0437c7948e2e321694ac5e2bd6548f66c0a68040
Mehrad Ansari <51170839+mehradans92@users.noreply.github.com>
2020-06-28 00:07:02 -0400

    Update try.py

--- examples/try.py
+++ examples/try.py
@@ -1,6 +1,8 @@
-from MaxEntEp.functions import *
-from MaxEntEp.SIR_model import *
+#from MaxEntEp.functions import *
 import numpy as np
+
+import MaxEntEp
+
 
 # SIR model
 S_zero, I_zero, R_zero = 0.999, 0.001, 0.0  # same starting amount exposed
@@ -9,7 +11,7 @@ SIR_params = {'alpha': 0.003,  # recovery rate
 
 N = 2000
 
-model = SIR_model([S_zero, I_zero, R_zero], SIR_params, N)
+model = MaxEntEp.SIR_model([S_zero, I_zero, R_zero], SIR_params, N)
 path = model()
 model.plot_trajectory()
 
@@ -19,15 +21,21 @@ path[500]
 e = 'laplace'
 e_params = {'sigma': 0.0005}
 eta = 1.5
+
+
 def a(): return np.abs(np.random.normal(loc=0.0025, scale=0.005))
+
+
 def b(): return np.abs(np.random.normal(loc=0.017, scale=0.004))
+
+
 def g(x): return x[500][1]
 
 
-reweighter = SIR_maxent_reweighter(
+reweighter = MaxEntEp.SIR_maxent_reweighter(
     200, 2000, g, [0.5617289], a, b, e, e_params, 10000, eta, 0.01)
 
 results, grad_terms, g_k_vals = reweighter.run(debug=True)
 
 traj = reweighter.make_plot(results, fixed_points=[[500, 0.5617289]])
-plt.show()
+
