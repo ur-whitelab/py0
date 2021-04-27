d4c4ab8e84d880e4bf93aed8b3acc444d783c5a5
Andrew White <white.d.andrew@gmail.com>
2021-04-27 16:14:35 -0400

    Added maxent to setup

--- setup.py
+++ setup.py
@@ -27,14 +27,12 @@ setup(
 
     packages=find_packages(exclude=('tests',)),
 
-    install_requires=['numpy', 'matplotlib', 'scipy', 'tqdm'],
+    install_requires=['numpy', 'matplotlib', 'scipy', 'tqdm', 'maxent@https://api.github.com/repos/ur-whitelab/maxent/tarball/master',],
 
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
