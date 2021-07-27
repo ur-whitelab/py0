04b8f6f3cf84ae68681f1f05a349ae8bab8195a6
Mehrad Ansari <mehrad.ansari@rochester.edu>
2021-07-27 16:34:19 -0400

    removed SIR test

--- tests/test_maxentep.py
+++ tests/test_maxentep.py
@@ -13,28 +13,6 @@ class TestFunctionality(unittest.TestCase):
         except ImportError as error:
             raise error
 
-class TestSIR(unittest.TestCase):
-    def test_SIR_model(self):
-        # Testing for zero epidemiology parameters
-        import py0
-        from py0 import SIRModel
-        import sys
-        N = 100
-        L = 30
-        trajs = np.empty((N, L, 3))
-        alphas = np.zeros(N)
-        betas = np.ones(N)
-        infected = ss.beta.rvs(1, 10, size = N)
-        susceptible = ss.beta.rvs(5, 2, size = N)
-        for i, a, b, I, S in zip(range(N), alphas, betas, infected, susceptible):
-            model = SIRModel([S, I, 1 - S - I], a, b, L)
-            trajs[i] = model()
-        grad = np.gradient(trajs[:, :, 1], axis=1)
-        np.set_printoptions(threshold=sys.maxsize)
-        print(trajs)
-        print(np.count_nonzero(grad, axis=None) == 1)
-        assert np.count_nonzero(grad, axis = None) == 1
-
 class TestMetpop(unittest.TestCase):
     def test_metapop_model(self):
         import py0
