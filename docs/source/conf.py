4644a7d28f83b2c172c58594c3eb2730a89d7b6f
Mehrad Ansari <51170839+mehradans92@users.noreply.github.com>
2021-08-25 10:05:45 -0400

    added edit on github (#36)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,4 +64,10 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_context = {
+    "display_github": True,  # Add 'Edit on Github' link instead of 'View page source'
+    "last_updated": True,
+    "commit": False,
+}
+
 master_doc = 'toc'
